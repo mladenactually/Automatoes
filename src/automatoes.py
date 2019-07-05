4457ce7930f830d0885015b74a56d0ddf32db8e5
cigor99 <igi.l.1999@gmail.com>
2019-07-05 22:48:50 +0200

    Layout prozora izmenjen i dodata ikonica

--- src/automatoes.py
+++ src/automatoes.py
@@ -1,12 +1,14 @@
 from sys import argv, exit
 
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QStatusBar, QGridLayout, QLabel, QHBoxLayout, QTextEdit
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QStatusBar, QGridLayout, QLabel, QComboBox, QPushButton, QHBoxLayout
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super(MainWindow, self).__init__()
         self.app = app
         self.setWindowTitle("Automatoes")
+        self.setWindowIcon(QIcon('automato.png'))
         desktop = app.desktop()
         height = desktop.screenGeometry().height()
         width = desktop.screenGeometry().width()
@@ -27,20 +29,33 @@ class MainWindow(QMainWindow):
         myStatusBar.addWidget(label1, 1)
 
     def centralWidgetSetup(self):
-        widget_layout = QHBoxLayout()
         cent_widg = QWidget()
-        cent_widg.setLayout(widget_layout)
-        left_layout = QGridLayout()
+
+        layout = QGridLayout()
 
         lab = QLabel()
         lab.setPixmap(QPixmap('tlocrt.png'))
-        left_layout.addWidget(lab,0,0)
-        left = QWidget()
-        left.setLayout(left_layout)
+        lab.setFixedSize(640, 320)
+        layout.setAlignment(Qt.AlignCenter)
+        layout.addWidget(lab)
 
-        right = QTextEdit()
-        widget_layout.addWidget(left)
-        widget_layout.addWidget(right)
+        device_menu = QComboBox()
+        layout.addWidget(device_menu, 1, 0)
+        label = QLabel("Bills")
+        layout.addWidget(label, 2, 0)
+        water_button = QPushButton("Water Bill")
+        gas_button = QPushButton("Gas Bill")
+        electricity_button = QPushButton("Elactricity Bill")
+        internet_button = QPushButton("Internet Bill")
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(water_button)
+        button_layout.addWidget(gas_button)
+        button_layout.addWidget(electricity_button)
+        button_layout.addWidget(internet_button)
+        bts = QWidget()
+        bts.setLayout(button_layout)
+        layout.addWidget(bts, 3, 0)
+        cent_widg.setLayout(layout)
         self.setCentralWidget(cent_widg)
 
 
