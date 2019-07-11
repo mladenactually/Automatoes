300206e217ab364b544938b82b065bd3790153a6
Uros Nenadic <urkewtf@hotmail.com>
2019-07-11 20:41:12 +0200

    Dodati dugmici na tlocrt

--- src/view/mainwindow.py
+++ src/view/mainwindow.py
@@ -34,7 +34,7 @@ class MainWindow(QMainWindow):
         layout = QGridLayout()
 
         lab = QLabel()
-        lab.setPixmap(QPixmap('tlocrt.png'))
+        lab.setPixmap(QPixmap('tlocrt2.png'))
         lab.setFixedSize(640, 320)
         layout.setAlignment(Qt.AlignCenter)
         layout.addWidget(lab)
@@ -45,10 +45,84 @@ class MainWindow(QMainWindow):
 
         fridge = QPushButton('Fridge', lab)
         air_condition = QPushButton('AC', lab)
-        fridge.move(10, 10)
-        fridge.setFixedSize(20, 20)
-        air_condition.move(50, 50)
-        air_condition.setFixedSize(20, 20)
+        dish_washer = QPushButton('Dish \n Washer', lab)
+        oven = QPushButton('Oven', lab)
+        stove = QPushButton('Stove', lab)
+        microwave = QPushButton('Microwave', lab)
+        washing_machine = QPushButton('Washing \n machine', lab)
+        dryer = QPushButton('Dryer', lab)
+        tv = QPushButton('Tv', lab)
+        stereo = QPushButton('Stereo',lab)
+        computer = QPushButton('Computer', lab)
+        kitchen_light = QPushButton('Light', lab)
+        bathroom_light = QPushButton('Light', lab)
+        living_room_light = QPushButton('Light', lab)
+        first_room_light = QPushButton('Light', lab)
+        second_room_light = QPushButton('Light', lab)
+
+
+        #KITCHEN
+
+        #fridge
+        fridge.move(260, 30)
+        fridge.setFixedSize(40, 40)
+        #dish_washer
+        dish_washer.move(190,30)
+        dish_washer.setFixedSize(70,40)
+        #oven
+        oven.move(150, 30)
+        oven.setFixedSize(40, 40)
+        #stove
+        stove.move(150, 70)
+        stove.setFixedSize(40, 40)
+        #microwave
+        microwave.move(150, 110)
+        microwave.setFixedSize(70, 40)
+        #light
+        kitchen_light.move(210, 160)
+        kitchen_light.setFixedSize(40, 30)
+
+        #BATHROOM
+        #washin_machine
+        washing_machine.move(10, 30)
+        washing_machine.setFixedSize(70, 40)
+        #dryer
+        dryer.move(80, 30)
+        dryer.setFixedSize(60, 40)
+        #light
+        bathroom_light.move(60, 100)
+        bathroom_light.setFixedSize(40, 30)
+
+
+        #LIVING ROOM
+
+        tv.move(10, 220)
+        tv.setFixedSize(20, 50)
+
+        stereo.move(95,195)
+        stereo.setFixedSize(50, 30)
+
+        # air_condition
+        air_condition.move(270, 240)
+        air_condition.setFixedSize(30, 50)
+
+        living_room_light.move(60, 230)
+        living_room_light.setFixedSize(40, 30)
+
+        #ROOMS
+
+        computer.move(455, 260)
+        computer.setFixedSize(57, 30)
+
+        first_room_light.move(340, 160)
+        first_room_light.setFixedSize(40, 30)
+
+        second_room_light.move(450, 160)
+        second_room_light.setFixedSize(40, 30)
+
+
+
+
         # дакле, када правимо QPushButton дугме, ставимо да му је родитељ
         # lab (тј. слика тлоцрта) тако што ту лабелу проследимо као други аргумент
         # онда можемо ручно да му подесимо димензије (setFixedSize) и позицију (move)
