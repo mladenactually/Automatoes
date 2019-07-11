908417d74ff6c8c18a594dc075795ada7632c2ca
cigor99 <igi.l.1999@gmail.com>
2019-07-11 20:46:09 +0200

    Izmenjene view i model klase

--- src/model/application.py
+++ src/model/application.py
@@ -1,2 +1,28 @@
 class Application:
-    pass
+    def __init__(self, smart_home, user):
+        self.smart_home = smart_home
+        self.user = user
+        self.user_data = {}
+
+    def login(self, username, password):
+        if username in self.user_data:
+            if self.user_data[username] == password:
+                return
+            else:
+                print("Invalid password")
+        else:
+            print("Username doesn't exist")
+
+    def logout(self):
+        pass
+
+    def load_user_data(self):
+        file = open('user_data', 'r')
+        line = file.readline()
+        while line != "":
+            tokens = line.split("|")
+            username = tokens[0]
+            password = tokens[1]
+            self.user_data[username] = password
+            line = file.readline()
+        file.close()
